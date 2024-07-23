165e61c2215b0089454aac7ea686e9fb5f7cf77e
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-07-23 13:42:46 -0300

    FINALIZANDO O DOM DO PROJETO TODOLIST

--- SECTION 8/TODOLIST/app.js
+++ SECTION 8/TODOLIST/app.js
@@ -1,7 +1,32 @@
+const container = document.querySelector(".container");
+const listaAtividades = document.querySelector(".lista_atividades");
+const input = document.querySelector(".input")
+const erro = document.querySelector(".erro")
+const botaoCadastra = document.querySelector(".botao_adc")
+const paleta1 = document.querySelector("#paleta1")
+const paleta2 = document.querySelector("#paleta2")
+const paleta3 = document.querySelector("#paleta3")
+const botaoDelTodos = document.querySelector('.botao_del_todos')
+
+
+function criaAtividade(){
+    const atividade = document.createElement("div")
+    atividade.classList.add("atividade")
+    const nomeAtividade = document.createElement("p")
+    nomeAtividade.textContent = input.value;
+    const botaoLimpar = document.createElement("button")
+    botaoLimpar.textContent = "Limpar"
+    botaoLimpar.classList.add("botao_del")
+    botaoLimpar.addEventListener('click', () => removeUmaAtividade(atividade))
+    listaAtividades.appendChild(atividade)
+    atividade.appendChild(nomeAtividade)
+    atividade.appendChild(botaoLimpar)
+}
+
 function cadastraAtividade(){
     if(input.value.length > 3){
         erro.style.display = "none";
-        //criaAtividade();
+        criaAtividade();
     }else{
         erro.style.display = "grid";
         erro.innerHTML = `${input.value} não é uma atividade válida!`
@@ -13,8 +38,36 @@ function limpaInput(){
     input.value = "";
 }
 
+function limpaLista(){
+    input.value = "";
+    while(listaAtividades.firstElementChild){
+        listaAtividades.removeChild(listaAtividades.firstElementChild)
+    }
+}
+
 window.addEventListener("keypress", (e) => {
     if(e.key === "Enter"){
         cadastraAtividade();
     }
 });
+
+botaoDelTodos.addEventListener('click', () => limpaLista())
+
+paleta1.addEventListener('mousemove', () => definePaleta('seagreen'))
+
+
+paleta2.addEventListener('mousemove', () => definePaleta('slateblue'))
+
+
+paleta3.addEventListener('mousemove', () => definePaleta('tomato'))
+
+botaoCadastra.addEventListener('click', () => cadastraAtividade())
+
+function removeUmaAtividade(atividade){
+    listaAtividades.removeChild(atividade)
+}
+
+function definePaleta(cor){
+    container.style.background = cor;
+    listaAtividades.style.background = cor;
+}
